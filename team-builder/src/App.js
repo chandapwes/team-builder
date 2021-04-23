f6407d179650b2b01c17ff168ef0c6a6e541c908
chandapwes <67353485+chandapwes@users.noreply.github.com>
2021-04-23 11:47:08 -0600

    what i could accomplish

---
+++ team-builder/src/App.js
@@ -0,0 +1,18 @@
+import logo from './logo.svg';
+import './App.css';
+import { React, useState } from 'react'
+
+const initialFormValues = {
+  name: '',
+  email: '',
+  role: ''
+}
+
+function App() {
+  const [teamList, setTeamList] = useState(null);
+
+
+  return null;
+}
+
+export default App;
