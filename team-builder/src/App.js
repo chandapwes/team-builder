7901309e28463d9a39dd55ddd4c274a78ec0a4d7
chandapwes <67353485+chandapwes@users.noreply.github.com>
2021-05-19 16:04:41 -0600

    MVP

--- team-builder/src/App.js
+++ team-builder/src/App.js
@@ -1,6 +1,12 @@
-import logo from './logo.svg';
-import './App.css';
-import { React, useState } from 'react'
+import { render } from 'react-dom'
+import React, { useState } from 'react'
+
+
+const teamList = [
+  { name: 'Chan Miner', email: 'chan@chan.com', role: 'student' },
+  { name: 'Alli Miner', email: 'alli@alli.com', role: 'student' },
+  { name: 'Micah Clement', email: 'micah@micah.com', role: 'teacher' }
+]
 
 const initialFormValues = {
   name: '',
@@ -9,10 +15,74 @@ const initialFormValues = {
 }
 
 function App() {
-  const [teamList, setTeamList] = useState(null);
+  const [members, setMembers] = useState(teamList);
+  const [formValues, setFormValues] = useState(initialFormValues);
 
+  const change = (ev) => {
+    const { name, value } = ev.target
+    setFormValues({...formValues, [name]: value})
+  }
 
-  return null;
+  const submit = (ev) => {
+    ev.preventDefault()
+    const newMember = {
+      name: formValues.name.trim(),
+      email: formValues.email.trim(),
+      role: formValues.role.trim()
+    }
+    setMembers([...members, newMember])
+    setFormValues(initialFormValues)
+  }
+
+return(
+  <div className='container'>
+    <h1>Team List</h1>
+    {members.map((member, idx) => {
+      return (
+        <div key={idx}>
+         Name: {member.name} 
+         <br />
+         Email: {member.email}
+         <br />
+         Role: {member.role}
+         <br />
+         <br />
+        </div>
+      )
+    })}
+    <form onSubmit={submit}>
+      <input
+      placeholder='name...'
+      type='text'
+      onChange={change}
+      value={formValues.name}
+      name='name'
+      />
+      <input 
+      placeholder='email...'
+      type='text'
+      onChange={change}
+      value={formValues.email}
+      name='email'
+      />
+      <input 
+      placeholder='role...'
+      type='text'
+      onChange={change}
+      value={formValues.role}
+      name='role'
+      />
+      <button>Submit</button>
+    </form>
+  </div>
+ )
 }
+
+render(
+  <>
+  <App />
+  </>
+  , document.querySelector('#root')
+)
 
 export default App;
